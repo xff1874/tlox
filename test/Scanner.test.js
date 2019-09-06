a49ba88123ee34e41c548ef8b1bb23b2dec7d66b
zwd <zwd321081@gmail.com>
2019-09-06 16:23:32 +0800

    feat: pss  scanner test

--- test/Scanner.test.js
+++ test/Scanner.test.js
@@ -1,30 +1,32 @@
 /* global describe, it */
 // import Scanner from "./build/Scanner"
-const fs = require('fs')
-const assert = require('assert')
-const Scanner = require('../build/Scanner')
+const fs = require('fs');
+const path = require('path');
+const Scanner = require('../build/Scanner');
+const TokenType = require("../build/TokenType");
 
+debugger;
 test('produces tokens for basic arithmetic', () => {
     const source = fs.readFileSync('./examples/simple.lox', 'utf-8')
 
-    const tokens = new Scanner(source).scanTokens()
-    expect(
-      tokens.map(t => t.type),
-      [
-        'PRINT',
-        'LEFT_PAREN',
-        'NUMBER',
-        'PLUS',
-        'NUMBER',
-        'RIGHT_PAREN',
-        'STAR',
-        'LEFT_PAREN',
-        'NUMBER',
-        'MINUS',
-        'NUMBER',
-        'RIGHT_PAREN',
-        'SEMICOLON',
-        'EOF'
+    const tokens = new Scanner.default(source).scanTokens();
+    let expectedArr=[
+        TokenType.default.PRINT,
+        TokenType.default.LEFT_PAREN,
+        TokenType.default.NUMBER,
+        TokenType.default.PLUS,
+        TokenType.default.NUMBER,
+        TokenType.default.RIGHT_PAREN,
+        TokenType.default.STAR,
+        TokenType.default.LEFT_PAREN,
+        TokenType.default.NUMBER,
+        TokenType.default.MINUS,
+        TokenType.default.NUMBER,
+        TokenType.default.RIGHT_PAREN,
+        TokenType.default.SEMICOLON,
+        TokenType.default.EOF
       ]
-    )
+    for(let i = 0;i<tokens.length;i++){
+        expect(tokens[i].type).toBe(expectedArr[i])
+    }
   })
