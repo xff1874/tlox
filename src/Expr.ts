dfc4bdb8aa479fc00e6389683b5da5c9993d3f39
zwd <zwd321081@gmail.com>
2019-09-09 10:55:38 +0800

    feat: add generatorast.js

--- src/Expr.ts
+++ src/Expr.ts
@@ -1,1 +1,36 @@
+import Token from "./Token";
 abstract class Expr {}
+export class Binary extends Expr {
+  constructor(left: Expr, operator: Token, right: Expr) {
+    super();
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
+  left: Expr;
+  operator: Token;
+  right: Expr;
+}
+export class Grouping extends Expr {
+  constructor(expression: Expr) {
+    super();
+    this.expression = expression;
+  }
+  expression: Expr;
+}
+export class Literal extends Expr {
+  constructor(value: Object) {
+    super();
+    this.value = value;
+  }
+  value: Object;
+}
+export class Unary extends Expr {
+  constructor(operator: Token, right: Expr) {
+    super();
+    this.operator = operator;
+    this.right = right;
+  }
+  operator: Token;
+  right: Expr;
+}
--- src/GenerateAst.js
+++ src/GenerateAst.js
@@ -1,19 +1,50 @@
 const fs = require("fs");
 const path = require("path");
 
+function defineType(stream, baseName, className, fields) {
+  stream.write(` export class ${className} extends ${baseName} { \n`);
+  //constructor
+  let filedCollection = fields.split(",");
+  let cstr = "";
+  for (let f of filedCollection) {
+    let name = f.trim().split(" ");
+    cstr += ` ${name[1]}:${name[0]}, `;
+  }
 
+  stream.write(`  constructor(${cstr}) { \n`);
+  stream.write(` super(); \n`);
+  for (let f of filedCollection) {
+    let name = f.trim().split(" ");
+    stream.write(`  this.${name[1]} = ${name[1]}; \n`);
+  }
+  stream.write("      } \n");
+  //处理field
+  for (let fv of filedCollection) {
+    let name = fv.trim().split(" ");
+    stream.write(`${name[1]}:${name[0]}; \n`);
+  }
 
-function defineAst(baseName,types){
-    let tsFile = path.join(__dirname,`${baseName}.ts`);
-    let stream = fs.createWriteStream(tsFile,{flag:'a'});
-    stream.write("abstract class " + baseName + " {");
-    stream.write("}")
-    stream.end();
+  stream.write("  } \n");
 }
 
-defineAst("Expr", [         
-    "Binary   : Expr left, Token operator, Expr right",
-    "Grouping : Expr expression",                      
-    "Literal  : Object value",                         
-    "Unary    : Token operator, Expr right"            
+function defineAst(baseName, types) {
+  let tsFile = path.join(__dirname, `${baseName}.ts`);
+  let stream = fs.createWriteStream(tsFile, { flag: "a" });
+  stream.write(`import Token from "./Token";\n `);
+  stream.write("abstract class " + baseName + " {");
+  stream.write("} \n");
+  for (let type of types) {
+    let className = type.split(":")[0].trim();
+    let fields = type.split(":")[1].trim();
+    defineType(stream, baseName, className, fields);
+  }
+
+  stream.end();
+}
+
+defineAst("Expr", [
+  "Binary   : Expr left, Token operator, Expr right",
+  "Grouping : Expr expression",
+  "Literal  : Object value",
+  "Unary    : Token operator, Expr right"
 ]);
