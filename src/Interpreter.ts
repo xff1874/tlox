291adc808ede38b294ccba155e9f4998bbd3e24f
zwd <zwd321081@gmail.com>
2019-09-16 14:46:33 +0800

    feat: finish interpter

--- src/Interpreter.ts
+++ src/Interpreter.ts
@@ -1,5 +1,6 @@
 import { Visitor, Binary, Grouping, Literal, Unary, Expr } from "./Expr";
 import TokenType from "./TokenType";
+import Token from "./Token";
 
 class Interpreter implements Visitor<Object> {
   visitBinaryExpr(expr: Binary): Object {
@@ -8,10 +9,13 @@ class Interpreter implements Visitor<Object> {
 
     switch (expr.operator.type) {
       case TokenType.MINUS:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) - Number(right);
       case TokenType.SLASH:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) / Number(right);
       case TokenType.STAR:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) * Number(right);
       case TokenType.PLUS:
         if (left instanceof Number && right instanceof Number) {
@@ -19,13 +23,18 @@ class Interpreter implements Visitor<Object> {
         } else if (left instanceof String && right instanceof String) {
           return String(left) + String(right);
         }
+        throw new Error(`${expr.operator} Operands must be numbers or string`);
       case TokenType.GREATER:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) > Number(right);
       case TokenType.GREATER_EQUAL:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) >= Number(right);
       case TokenType.LESS:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) < Number(right);
       case TokenType.LESS_EQUAL:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) <= Number(right);
       case TokenType.BANG_EQUAL:
         return !this.isEqual(left, right);
@@ -67,5 +76,23 @@ class Interpreter implements Visitor<Object> {
     if (object == null) return false;
     if (object instanceof Boolean) return Boolean(object);
     return true;
+  }
+  checkNumberOperand(operator: Token, operand: Object) {
+    if (operand instanceof Number) return;
+    throw new Error(`${operator} operand must be a number`);
+  }
+
+  checkNumberOperands(operator: Token, left: Object, right: Object) {
+    if (left instanceof Number && right instanceof Number) return;
+    throw new Error(`${operator} Oprands must be number`);
+  }
+
+  interpret(expression: Expr) {
+    try {
+      let val = this.evaluate(expression);
+      console.log(JSON.stringify(val));
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
